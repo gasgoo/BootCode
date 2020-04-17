ddb9cbd5ffd8687fa723cde507ec21fb075d80a9
raogugen <raogugen@magfin.cn>
2020-04-17 16:51:54 +0800

    add nacos Register

---
+++ nacos-common/src/main/java/com/api/register/NacosCommonApplication.java
@@ -0,0 +1,13 @@
+package com.api.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosCommonApplication.class, args);
+    }
+
+}
---
+++ nacos-common/src/test/java/com/api/register/NacosCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
